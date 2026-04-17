a5178023530c62cd38941f26f386014bbd7f89d9
David Ventura <david@davidv.dev>
2026-04-17 11:35:07 +0200

    sleep

--- run_phone.sh
+++ run_phone.sh
@@ -4,5 +4,5 @@ set -e
 ./venv/bin/clickable install -c clickable.yaml -a arm64  --serial-number 93BAY08W6S  
 sleep 0.2
 ./venv/bin/clickable launch -c clickable.yaml -a arm64  --serial-number 93BAY08W6S  
-sleep 0.5
+sleep 2
 ./venv/bin/clickable logs -c clickable.yaml -a arm64  --serial-number 93BAY08W6S  
